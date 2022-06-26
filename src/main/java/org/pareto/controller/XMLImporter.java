1113f54e3435727d9ae7423fedc341f5b6dd385b
heshsum <mail@heshsum.de>
2022-06-26 19:09:25 +0200

    Added private Constructor

--- src/main/java/org/pareto/controller/XMLImporter.java
+++ src/main/java/org/pareto/controller/XMLImporter.java
@@ -11,18 +11,16 @@ package org.pareto.controller;
 
 import java.io.File;
 import java.io.IOException;
-
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
-
 import org.pareto.model.Book;
+import static org.pareto.controller.LogHelper.log;
 
 /**
  * Class generate Book entries from an XML export file.
@@ -32,6 +30,13 @@ import org.pareto.model.Book;
  * @author   Hendrik Schlange (mail@hendrikschlange.de)
  */
 public class XMLImporter {
+
+    /**
+     * Private Constructor.
+     */
+    private XMLImporter() {
+        log("Utility class");
+    }
 
     /**
      * Method to import Book object via XML file import.
